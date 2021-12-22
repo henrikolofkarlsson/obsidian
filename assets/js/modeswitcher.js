85e27b20eb03091ad6ee3897a63b0b3d94bfcce2
Martin Roberts <mroberts1@gmail.com>
2021-12-22 00:58:38 +0000

    Trying to fix baseurl problem

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -13,19 +13,19 @@ function changeIconImgSrc(src) {
 }
 
 if (systemInitiatedDark.matches) {
-	changeIconImgSrc("../assets/img/moon.svg");
+	changeIconImgSrc({{ "/assets/img/moon.svg" | relative_url }});
 } else {
-	changeIconImgSrc("../assets/img/sun.svg");
+	changeIconImgSrc({{ "/assets/img/sun.svg" | relative_url }});
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	changeIconImgSrc("../assets/img/moon.svg");
+   	changeIconImgSrc({{ "/assets/img/moon.svg" | relative_url }});
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    changeIconImgSrc("../assets/img/sun.svg");
+    changeIconImgSrc({{ "/assets/img/sun.svg" | relative_url }});
     sessionStorage.setItem('theme', '');
   }
 }
@@ -37,28 +37,28 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc("/assets/img/sun.svg");
+		changeIconImgSrc({{ "/assets/img/sun.svg" | relative_url }});
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc("../assets/img/moon.svg");
+		changeIconImgSrc({{ "/assets/img/moon.svg" | relative_url }});
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		changeIconImgSrc("../assets/img/sun.svg");
+		changeIconImgSrc({{ "/assets/img/sun.svg" | relative_url }});
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		changeIconImgSrc("../assets/img/moon.svg");
+		changeIconImgSrc({{ "/assets/img/moon.svg" | relative_url }});
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	changeIconImgSrc("../assets/img/moon.svg");
+	changeIconImgSrc({{ "/assets/img/moon.svg" | relative_url }});
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	changeIconImgSrc("../assets/img/sun.svg");
+	changeIconImgSrc({{ "/assets/img/sun.svg" | relative_url }});
 }
